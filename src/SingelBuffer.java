51b457317e5efc4fd77494ae33584f51976efbb7
samuelort <ort.samuel@gmail.com>
2015-03-31 13:16:19 +0200

    Uppgift 1, klar.

---
+++ src/SingelBuffer.java
@@ -0,0 +1,31 @@
+/**
+ * Created by samuelort on 15-03-31.
+ * Project name: Labb2
+ */
+public class SingelBuffer<E> {
+    Object [] arr = (E[]) new Object [1];
+
+
+    public SingelBuffer() {
+        arr[0] = null;
+    }
+    public boolean put(E e){
+
+        if(arr[0] == null){
+            arr[0] = e;
+            return true;
+        }else
+            return false;
+    }
+
+    public E get(){
+        if(arr[0] == null){
+            return null;
+        }
+        E ee = null;
+        ee = (E) arr[0];
+        arr[0] = null;
+        return ee;
+    }
+
+}
--- src/Test.java
+++ src/Test.java
@@ -4,9 +4,16 @@
  */
 public class Test {
     public static void main(String[] args) {
-
-
-
-        asda
+        SingelBuffer<Integer> b = new SingelBuffer<Integer>();
+        int x,y;
+        boolean res1,res2,res3;
+        res1 = b.put(123);
+        res2 = b.put(345);
+        x = b.get();
+        res3 = b.put(456);
+        System.out.println(res1);
+        System.out.println(res2);
+        System.out.println(x);
+        System.out.println(res3);
     }
 }
